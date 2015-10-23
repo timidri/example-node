5538f6ea18a5517f347dccb53287adaec02b6436
Brian McGehee <doc@tavian.com>
2015-10-23 16:44:05 -0700

    newrelic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var newRelic = require('newrelic');
 
 var timeController = require('./controllers/time');
 var homeController = require('./controllers/home');
